c19dd0d1a82d3f86cb37a91de83049ef0c9b1afe
Pat Shaughnessy <pat@patshaughnessy.net>
2011-02-17 12:46:48 -0500

    specs and erroneous_call_to_proc check

--- lib/streamer.rb
+++ lib/streamer.rb
@@ -1,33 +1,38 @@
 module Streamer
-  
+
+  mattr_accessor :erroneous_call_to_proc
   mattr_accessor :first_record
-  
+
   def stream_json
     self.first_record = true
     headers["Content-Type"] = "application/json"
+    self.erroneous_call_to_proc = false
     self.response_body = proc { |response, output| 
-      @@output = output
-      @@output.write '['
-      yield(output)
-      @@output.write ']'
+      unless self.erroneous_call_to_proc
+        @@output = output
+        @@output.write '['
+        yield(output)
+        @@output.write ']'
+      end
+      self.erroneous_call_to_proc = true
     }
   end
-  
+
   def send_record(c)
     self.first_record  ? self.first_record = false : @@output.write(',')
     @@output.write c.to_json
   end
-  
-  
-  
+
   def stream_to(file_name)
     headers["Content-Type"] = "text/plain"
     headers["Content-disposition"] = "attachment; filename=\"#{file_name}\""
+    self.erroneous_call_to_proc = false
     self.response_body = proc { |response, output|
-      yield(output)
+      unless self.erroneous_call_to_proc
+        yield(output)
+      end
+      self.erroneous_call_to_proc = true
     }
   end
-  
+
 end
- 
- 
--- spec/streamer_spec.rb
+++ spec/streamer_spec.rb
@@ -1,7 +1,89 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+class FakeController
+  include Streamer
+  attr_accessor :headers
+  attr_accessor :response_body
+  def initialize
+    @headers = {}
+  end
+end
+
 describe Streamer do
-  it 'should have some unit tests' do
-    fail 'to do: write some real tests here'
+
+  describe '#stream_to' do
+
+    before do
+      @controller = FakeController.new
+      @controller.stream_to('test_file_name.txt') do |output|
+        output.write('Some data to stream to the client...')
+      end
+    end
+
+    it 'set the content type header to text/plain' do
+      @controller.headers["Content-Type"].should == "text/plain"
+    end
+
+    it 'set the content type header to text/plain' do
+      @controller.headers["Content-disposition"].should == "attachment; filename=\"test_file_name.txt\""
+    end
+
+    it 'should set the response body to a proc which yields the output stream' do
+      body = @controller.response_body
+      body.is_a?(Proc).should be_true
+      mock_response = mock
+      mock_response.expects(:write).with('Some data to stream to the client...')
+      body.call(mock, mock_response)
+    end
+
+    it 'should ignore the erroneous second call to the response proc' do
+      body = @controller.response_body
+      mock_response = mock
+      mock_response.expects(:write).with('Some data to stream to the client...')
+      body.call(mock, mock_response)
+      body.call(mock, mock_response)
+    end
+  end
+
+  describe '#stream_json' do
+    before do
+      @controller = FakeController.new
+    end
+
+    it 'set the content type header to application/json' do
+      @controller.stream_json
+      @controller.headers["Content-Type"].should == "application/json"
+    end
+
+    describe 'for two mock objects' do
+
+      before do
+        (value1 = mock).expects(:to_json).returns('JSON_FOR_VALUE1')
+        (value2 = mock).expects(:to_json).returns('JSON_FOR_VALUE2')
+        @controller.stream_json do |output|
+          [value1, value2].each do |value|
+            @controller.send_record(value)
+          end
+        end
+        @mock_response = mock
+        @mock_response.expects(:write).with('[')
+        @mock_response.expects(:write).with("JSON_FOR_VALUE1")
+        @mock_response.expects(:write).with(",")
+        @mock_response.expects(:write).with("JSON_FOR_VALUE2")
+        @mock_response.expects(:write).with(']')
+      end
+
+      it 'should set the response body to a proc which yields the given values as json' do
+        body = @controller.response_body
+        body.is_a?(Proc).should be_true
+        body.call(mock, @mock_response)
+      end
+
+      it 'should ignore the erroneous second call to the response proc' do
+        body = @controller.response_body
+        body.call(mock, @mock_response)
+        body.call(mock, @mock_response)
+      end
+    end
   end
 end
